f1cb1f4c587ed1bd8dc109bd5bab0d0021814bf3
sylvexn <sylvexn@gmail.com>
2025-10-30 10:41:01 -0400

    Added samples, evaluate headers properly, & validating data type

--- tests/features/test_metrics.py
+++ tests/features/test_metrics.py
@@ -6,9 +6,15 @@ from prometheus_client.parser import text_string_to_metric_families
 def test_metric_check_ok(client):
     """Test case for a successful metrics check."""
     response = client.get("/metrics")
+    content_type = response.headers.get("Content-Type")
     assert response.status_code == 200
-    assert response.headers["content-type"] == CONTENT_TYPE_LATEST
+    assert content_type == CONTENT_TYPE_LATEST
 
-    families = {mf.name: mf for mf in text_string_to_metric_families(response.text)}
+    body = response.get_data(as_text=True)
+    families = {mf.name: mf for mf in text_string_to_metric_families(body)}
     assert "process_uptime_seconds" in families
     assert "build_info_info" in families
+
+    assert any(True for _ in families["process_uptime_seconds"].samples)
+    assert any(True for _ in families["build_info_info"].samples)
+
