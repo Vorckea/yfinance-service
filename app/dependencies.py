1a97ec5735f4be65071a01a14dfb1df3a5fe1abc
Abhishikth Daniel Merugu <78458132+AbhishikthDaniel@users.noreply.github.com>
2025-12-17 20:43:24 -0800

    fixed bugs
    fixed typo on import statement and updated TTLCache initialization to pass size, cache_name and resource

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 
 from .clients.interface import YFinanceClientInterface
 from .clients.yfinance_client import YFinanceClient
-from .utils.cache. import TTLCache
+from .utils.cache import TTLCache
 
 @lru_cache
 def get_yfinance_client() -> YFinanceClientInterface:
@@ -16,4 +16,4 @@ def get_yfinance_client() -> YFinanceClientInterface:
 def get_info_cache() -> TTLCache:
     """Get a shared TTL cache for info responses (company metadata is relatively stable)."""
     # 5-minute TTL for info; quote data is fetched fresh each time.
-    return TTLCache(maxsize=256, ttl=300)
+    return TTLCache(size=256, ttl=300, cache_name="tll_cache", resource="snapshot")
